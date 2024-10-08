dcac867484ad550c48d6d8f469cb544609e584fc
Yongye Tan <yongyetan1209@gmail.com>
2024-10-08 00:27:48 -0400

    Initial Commit

---
+++ FetchSubmission/FetchSubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchSubmissionApp.swift
+//  FetchSubmission
+//
+//  Created by Yongye on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchSubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
